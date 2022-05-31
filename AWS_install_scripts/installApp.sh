be9e9f3b00076b1ddebf9af2c0880c6e14824c1f
Henning Seip <Henning.Seip@candogram.com>
2022-05-30 21:34:04 -0400

    added echos to the installApp.sh

--- AWS_install_scripts/installApp.sh
+++ AWS_install_scripts/installApp.sh
@@ -19,7 +19,8 @@ fi
 #----------------------------------
 cd /home/ubuntu/BASE/client
 npm install
+echo "Quasar build step"
 quasar build
-
+echo "start Node server..."
 #start Node server for social media apis
 systemctl start nodejs
